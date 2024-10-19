a938e204fe978232e903a30bc080e30c0fcd9256
tekmilesahin14 <tekmilesahin8@gmail.com>
2024-10-19 18:34:30 +0300

    first commit

---
+++ MyPortfolio/DAL/Entities/Feature.cs
@@ -0,0 +1,9 @@
+﻿namespace MyPortfolio.DAL.Entities
+{
+    public class Feature
+    {
+        public int FeatureId { get; set; }
+        public String Title { get; set; }
+        public String Description { get; set; }
+    }
+}
